9d39d5c3705e80880681c2634a9da04eefb27dad
Sankalp Tiwari <sankalp20tiwari@gmail.com>
2025-01-15 17:33:41 +0530

    get message route and layout

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,8 +11,8 @@ export async function middleware(request: NextRequest) {
   if (token && (url.pathname.startsWith('/sign-in')
         || url.pathname.startsWith('/sign-up')
         || url.pathname.startsWith('/verify')
-        || url.pathname.startsWith('/')
-  )) {
+        || url.pathname === '/')
+  ) {
     return NextResponse.redirect(new URL('/dashboard', request.url))
   }
   if (!token && url.pathname.startsWith('/dashboard')) {
